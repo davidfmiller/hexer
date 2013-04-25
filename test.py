0d4483068f164b705b7187ca3a08fed2b747aae3
David Miller <davidfmiller@me.com>
2013-04-25 02:44:36 -0700

    initial unit test

--- test.py
+++ test.py
@@ -10,14 +10,18 @@ import unittest
 class TestRGB(unittest.TestCase):
 
     def setUp(self):
+      self.black = hexer.RGB('#000')
+      self.white = hexer.RGB('#fff')
       pass
 
     def testHex(self):
-#      self.assertTrue(True)
+      self.assertEqual(self.black.hex(), '#000')
+      self.assertEqual(self.white.hex(), '#000')
 #      self.assertRaises(TypeError, random.shuffle, (1,2,3))
       pass
 
     def testRGB(self):
+      self.assertEqual(self.white.rgb(), 'rgb(0,0,0)')
 #      self.assertTrue(True)
       pass
 
