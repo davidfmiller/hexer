62aed9f5eb1783e94da40f14c9c58ab7889b657a
David Miller <davidfmiller@me.com>
2013-04-25 02:18:19 -0700

    baby steps

--- hexer.py
+++ hexer.py
@@ -1,44 +1,69 @@
 #!/usr/bin/python
 
 """
-A simple python script to toggle between hex (#rrggbb/#rgb) colour values and their rgb(rr,gg,bb) equivalents
+A simple python script to toggle between hex (#rrggbb or #rgb) colour values and their rgb(rr,gg,bb) equivalents
 """
 
 import sys
 
-class Colour:
+class RGB:
   """
   
+
   """
   def __init__(self, value):
+    """
+    
+    
+    @param value - the colour value to be parsed
+    @raise Exception if an invalid colour value is provided
+    """
 
-    self.value = value
+    self.isHex = False
+    self.red = self.blue = self.green = 0
 
     if (value[0] == '#'):
- #     print '#' + value
-      pass
+      self.isHex = True
+      if (len(value) == 4):
+        pass
+      elif (len(value) == 7):
+        pass
+      else:
+        raise Exception, 'Invalid hex colour: ' + value
+
     elif (value[0:4] == 'rgb('):
-#      print 'rgb(' + value + ')'
       pass
     else:
-      raise Error, value + "is not a valid colour\n"
+      raise Exception, value + "is not a valid colour"
 
+  def toggle(self):
+    """
+    @return string 
+    """
+    return self.rgb() if self.isHex else self.hex()
 
-  def toHex(self):
-    return '#' + value
+  def hex(self):
+    """
+    @return string - the colour declaration in #rrggbb format
+    """
+    return '#' + str(self.red) + str(self.blue) + str(self.green)
 
-  def toRGB(self):
-    return 'rgb(' + value + ')'
+  def rgb(self):
+    """
+    @return string - the colour declaration in rgb(rr,gg,bb) format
+    """
+    return 'rgb(' + str(self.red) + ',' + str(self.green) + ',' + str(self.blue) + ')'
 
   def __str__(self):
-    return self.value
-
-
+    return self.rgb()
 
 if len(sys.argv) == 1:
   sys.stderr.write("No colour specified\n")
+  sys.exit(1)
 
 for i in sys.argv[1:]:
-  c = Colour(i)
-  print c
-  
+  try:
+    c = RGB(i)
+    print c
+  except Exception, e:
+    print e
